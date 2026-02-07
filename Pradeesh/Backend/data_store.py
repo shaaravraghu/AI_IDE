c532bc2f997268c5754edc8c4d21c7848dcfe053
Pradeeshph23 <pradeeshpradeesh91@gmail.com>
2026-02-07 10:24:57 +0530

    Delete Pradeesh/Backend directory

--- Pradeesh/Backend/data_store.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-from models import Timeline, RaiseRequest, ReviewRequest, CommitHistory
-
-timelines: List[Timeline] = []
-raise_requests: List[RaiseRequest] = []
-review_requests: List[ReviewRequest] = []
-commit_history: List[CommitHistory] = []
